6480f9115fc16f2da81e0bbbec53338bbe439f1a
tailwindpartners <70972327+tailwindpartners@users.noreply.github.com>
2020-09-08 23:27:02 +0530

    Add files via upload

---
+++ precache-manifest.6a7026542ccd723ebf9df9c750a9341f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1264e4b16d4495b7d8342405c0d37595",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce95989f6d4e2b53e7cd",
+    "url": "/static/css/main.85517aea.chunk.css"
+  },
+  {
+    "revision": "92d8c5ac2c2ff1af2444",
+    "url": "/static/js/2.8073dfad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8073dfad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce95989f6d4e2b53e7cd",
+    "url": "/static/js/main.7ee2d52e.chunk.js"
+  },
+  {
+    "revision": "c7179b50da4b484ecb24",
+    "url": "/static/js/runtime-main.63c99784.js"
+  },
+  {
+    "revision": "422e766543a3850de8db1f049775a1e9",
+    "url": "/static/media/facebook.422e7665.svg"
+  },
+  {
+    "revision": "59b0c8cb832538da82cba3147939d41d",
+    "url": "/static/media/instagram.59b0c8cb.svg"
+  },
+  {
+    "revision": "89dcd6955d20dc3183df9d8ba408312c",
+    "url": "/static/media/linkedin.89dcd695.svg"
+  }
+]);
